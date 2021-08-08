08a668728119c82c90f7aa2345e5c982ecefbcab
ZFRenato <renatopscarlos@gmail.com>
2021-08-08 19:18:33 -0300

    Criação dos metodos size(), isEmpty(), getNo() e get()

---
+++ listaCirculares/src/main/java/one/digitalinnovation/ListaCircular.java
@@ -0,0 +1,47 @@
+package one.digitalinnovation;
+
+public class ListaCircular<T> {
+
+    private No<T> cabeca;
+    private No<T> cauda;
+    private int tamanhoLista;
+
+    public ListaCircular(){
+        this.tamanhoLista = 0;
+    }
+
+    // Metodos
+
+    public int size(){
+        return this.tamanhoLista;
+    }
+
+    public boolean isEmpty(){
+        return this.size() == 0;
+    }
+
+    private No<T> getNo(int index){
+        if(this.isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vazia");
+
+        if (index == 0){
+            return this.cauda;
+        }
+
+        No<T> noAuxiliar = this.cauda;
+        for (int i = 0; (i < index) && (noAuxiliar != null) ; i++) {
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+
+        return noAuxiliar;
+    }
+
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+
+
+
+
+}
